a44ad88c84f972a264955ae886aeaa9c756a2ed0
kinza-khan <kinzaaptech@gmail.com>
2023-11-29 09:13:18 +0500

    PWJS

---
+++ PWJS/Modules/main.js
@@ -0,0 +1,12 @@
+export let userName = 'ali';
+export let userEmail = 'ali@gmail.com';
+
+export let myFun = (n1,n2) =>{
+       return  n1 + n2 ;
+}
+
+export let myFun2 = ()=>{
+let uName = document.querySelector('#uName').value;
+let uEmail = document.querySelector('#uEmail').value;
+        return uName + ' has this ' + uEmail ;
+}
